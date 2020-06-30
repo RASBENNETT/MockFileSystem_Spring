6e9b456b8dc07ffb079a72de37552d4d48bdb30b
Ruairidh Benentt <ruairidhbennett@gmail.com>
2020-06-30 22:21:11 +0100

    Initial Commit

---
+++ src/main/java/com/example/codeclan/MockFileSystem/models/User.java
@@ -0,0 +1,56 @@
+package com.example.codeclan.MockFileSystem.models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="User")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @OneToMany(mappedBy = "user")
+    private List<Folder> folders;
+
+    public User(String name) {
+        this.name = name;
+        this.folders = new ArrayList<>();
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
+    }
+
+    public void addFolder(Folder folder){
+        this.folders.add(folder);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
